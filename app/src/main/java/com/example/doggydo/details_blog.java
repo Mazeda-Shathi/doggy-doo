4d01eb1f75687315612ce0a251da2466961d051c
Mazeda-Shathi <mazedaaktershathi2019@gmail.com>
2023-06-08 11:49:25 +0600

    Trying to committing

---
+++ app/src/main/java/com/example/doggydo/details_blog.java
@@ -0,0 +1,14 @@
+package com.example.doggydo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class details_blog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_blog);
+    }
+}
